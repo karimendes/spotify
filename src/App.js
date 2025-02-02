731998a84e4d7e592d698fe5ce34f2bdc82fad1d
Karine F. Mendes <karineferreiramendes@gmail.com>
2025-02-02 14:31:20 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Sidebar from '../src/components/Sidebar/Sidebar.js';
+import Main from '../src/components/Main/Main.js';
+import Footer from '../src/components/Footer/Footer.js';
+
+function App() {
+  return (
+    <div>
+    <Sidebar/>
+    <Main />
+    <Footer/>
+    </div>
+  );
+}
+
+export default App;
